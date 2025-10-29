86ad38410f9e52fa2574da4df227fcdb3248d168
Mihail Butvin <butvin.mihail@yandex.ru>
2025-10-29 18:07:59 +0300

    Use vectors to store polynomials coefficients

--- src/fhe.zig
+++ src/fhe.zig
@@ -8,7 +8,7 @@ pub fn QuotientPolyRing(d: comptime_int, K: type) type {
         const Self = @This();
 
         /// a_0 + a_1*x + a_2*x^2 + ... a_{d-1}*x^{d-1}
-        c: [d]K,
+        c: @Vector(d, K),
 
         pub fn add(self: *Self, other: Self) *Self {
             for (0..d) |i| self.c[i] += other.c[i];
@@ -21,7 +21,7 @@ pub fn QuotientPolyRing(d: comptime_int, K: type) type {
         }
 
         pub fn mul(self: *Self, other: Self) *Self {
-            var tmp: [2 * d - 1]K = .{0} ** (2 * d - 1);
+            var tmp: @Vector(2 * d - 1, K) = @splat(0);
             for (0..d) |i| {
                 if (self.c[i] == 0) continue;
                 for (0..d) |j| tmp[i + j] += self.c[i] * other.c[j];
@@ -34,31 +34,31 @@ pub fn QuotientPolyRing(d: comptime_int, K: type) type {
 }
 
 test "test add" {
-    const P = QuotientPolyRing(4, i4096);
+    const R = QuotientPolyRing(4, i4096);
 
-    var p1: P = .{ .c = .{ 0, 1, 2, 3 } };
-    const p2: P = .{ .c = .{ 4, 5, 6, 7 } };
+    var p1: R = .{ .c = .{ 0, 1, 2, 3 } };
+    const p2: R = .{ .c = .{ 4, 5, 6, 7 } };
 
     _ = p1.add(p2);
-    try testing.expectEqual(P{ .c = .{ 4, 6, 8, 10 } }, p1);
+    try testing.expectEqual(R{ .c = .{ 4, 6, 8, 10 } }, p1);
 }
 
 test "test sub" {
-    const P = QuotientPolyRing(4, i4096);
+    const R = QuotientPolyRing(4, i4096);
 
-    var p1: P = .{ .c = .{ 0, 1, 2, 3 } };
-    const p2: P = .{ .c = .{ 4, 5, 6, 7 } };
+    var p1: R = .{ .c = .{ 0, 1, 2, 3 } };
+    const p2: R = .{ .c = .{ 4, 5, 6, 7 } };
 
     _ = p1.sub(p2);
-    try testing.expectEqual(P{ .c = .{ -4, -4, -4, -4 } }, p1);
+    try testing.expectEqual(R{ .c = .{ -4, -4, -4, -4 } }, p1);
 }
 
 test "test mul" {
-    const P = QuotientPolyRing(4, i4096);
+    const R = QuotientPolyRing(4, i4096);
 
-    var p1: P = .{ .c = .{ 0, 1, 2, 3 } };
-    const p2: P = .{ .c = .{ 4, 5, 6, 7 } };
+    var p1: R = .{ .c = .{ 0, 1, 2, 3 } };
+    const p2: R = .{ .c = .{ 4, 5, 6, 7 } };
 
     _ = p1.mul(p2);
-    try testing.expectEqual(P{ .c = .{ -34, -28, -8, 28 } }, p1);
+    try testing.expectEqual(R{ .c = .{ -34, -28, -8, 28 } }, p1);
 }

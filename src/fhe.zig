1a97c8092cf26083f6497d4656a06e2d20c4f232
Mihail Butvin <butvin.mihail@yandex.ru>
2025-10-29 19:17:09 +0300

    Optimize tmp allocations, test for repeated operations

--- src/fhe.zig
+++ src/fhe.zig
@@ -11,23 +11,31 @@ pub fn QuotientPolyRing(d: comptime_int, K: type) type {
         c: @Vector(d, K),
 
         pub fn add(self: *Self, other: Self) *Self {
-            for (0..d) |i| self.c[i] += other.c[i];
+            self.c += other.c;
             return self;
         }
 
         pub fn sub(self: *Self, other: Self) *Self {
-            for (0..d) |i| self.c[i] -= other.c[i];
+            self.c -= other.c;
             return self;
         }
 
-        pub fn mul(self: *Self, other: Self) *Self {
-            var tmp: @Vector(2 * d - 1, K) = @splat(0);
+        pub fn mul(self: *Self, other: Self, tmp: *Self) *Self {
+            tmp.c = @splat(0);
+
             for (0..d) |i| {
                 if (self.c[i] == 0) continue;
-                for (0..d) |j| tmp[i + j] += self.c[i] * other.c[j];
+                for (0..d) |j| {
+                    const sum_idx = i + j;
+                    if (sum_idx < d) {
+                        tmp.c[sum_idx] += self.c[i] * other.c[j];
+                    } else {
+                        tmp.c[sum_idx - d] -= self.c[i] * other.c[j];
+                    }
+                }
             }
-            for (0..d - 1) |i| self.c[i] = tmp[i] - tmp[i + d];
-            self.c[d - 1] = tmp[d - 1];
+
+            self.c = tmp.c;
             return self;
         }
     };
@@ -41,6 +49,9 @@ test "test add" {
 
     _ = p1.add(p2);
     try testing.expectEqual(R{ .c = .{ 4, 6, 8, 10 } }, p1);
+
+    _ = p1.add(p2);
+    try testing.expectEqual(R{ .c = .{ 8, 11, 14, 17 } }, p1);
 }
 
 test "test sub" {
@@ -51,6 +62,9 @@ test "test sub" {
 
     _ = p1.sub(p2);
     try testing.expectEqual(R{ .c = .{ -4, -4, -4, -4 } }, p1);
+
+    _ = p1.sub(p2);
+    try testing.expectEqual(R{ .c = .{ -8, -9, -10, -11 } }, p1);
 }
 
 test "test mul" {
@@ -58,7 +72,11 @@ test "test mul" {
 
     var p1: R = .{ .c = .{ 0, 1, 2, 3 } };
     const p2: R = .{ .c = .{ 4, 5, 6, 7 } };
+    var tmp: R = .{ .c = @splat(0) };
 
-    _ = p1.mul(p2);
+    _ = p1.mul(p2, &tmp);
     try testing.expectEqual(R{ .c = .{ -34, -28, -8, 28 } }, p1);
+
+    _ = p1.mul(p2, &tmp);
+    try testing.expectEqual(R{ .c = .{ -32, -394, -572, -334 } }, p1);
 }

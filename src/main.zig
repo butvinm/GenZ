e5ec828200d4db7cf2fe2a1a7bd17c5b3ae885e6
Mihail Butvin <butvin.mihail@yandex.ru>
2025-11-09 09:02:24 +0300

    Add httpz to dependencies

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,28 @@
 const std = @import("std");
-const GenZ = @import("GenZ");
+const httpz = @import("httpz");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try GenZ.bufferedPrint();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    // More advance cases will use a custom "Handler" instead of "void".
+    // The last parameter is our handler instance, since we have a "void"
+    // handler, we passed a void ({}) value.
+    var server = try httpz.Server(void).init(allocator, .{ .port = 5882 }, {});
+    defer {
+        // clean shutdown, finishes serving any live request
+        server.stop();
+        server.deinit();
+    }
+
+    var router = try server.router(.{});
+    router.get("/api/user/:id", getUser, .{});
+
+    // blocks
+    try server.listen();
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+fn getUser(req: *httpz.Request, res: *httpz.Response) !void {
+    res.status = 200;
+    try res.json(.{ .id = req.param("id").?, .name = "Teg" }, .{});
 }

d0449160f8985f25a5f54e25b04abc1a329640ce
Mihail Butvin <butvin.mihail@yandex.ru>
2025-11-10 22:46:39 +0300

    Whatever

--- src/main.zig
+++ src/main.zig
@@ -1,29 +1,64 @@
 const std = @import("std");
 const httpz = @import("httpz");
+const uuid = @import("uuid");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    // More advance cases will use a custom "Handler" instead of "void".
-    // The last parameter is our handler instance, since we have a "void"
-    // handler, we passed a void ({}) value.
-    var server = try httpz.Server(void).init(allocator, .{ .address = "0.0.0.0", .port = 5882 }, {});
+    const address = "0.0.0.0";
+    const port = 5882;
+
+    var app = App{};
+    var server = try httpz.Server(*App).init(allocator, .{ .address = address, .port = port }, &app);
     defer {
-        // clean shutdown, finishes serving any live request
         server.stop();
         server.deinit();
     }
 
     var router = try server.router(.{});
-    router.get("/api/user/:id", getUser, .{});
+    router.post("/api/v0.1.0/register", register, .{});
+    // router.post("/api/v0.1.0/analyze", analyze, .{});
 
-    // blocks
-    std.debug.print("Server is listening on 0.0.0.0:5882", .{});
+    std.log.info("Server is listening on http://{[address]s}:{[port]}", .{ .address = address, .port = port });
     try server.listen();
 }
 
-fn getUser(req: *httpz.Request, res: *httpz.Response) !void {
+const App = struct {
+    pub fn savePublicKey(_: App, publicKey: []u8) !void {
+        const file = try std.fs.cwd().createFile("public_key.bin", .{});
+        defer file.close();
+        try file.writeAll(publicKey);
+    }
+};
+
+const RegisterRequest = struct {
+    public_key: []u8,
+
+    pub fn validateRequest(alloc: std.mem.Allocator, req: *httpz.Request) anyerror!RegisterRequest {
+        const request_raw = try req.json(struct { public_key: []u8 }) orelse return error.ValidationError;
+        const decodedSize = try std.base64.standard.Decoder.calcSizeForSlice(request_raw.public_key);
+        const request = RegisterRequest{ .public_key = try alloc.alloc(u8, decodedSize) };
+        try std.base64.standard.Decoder.decode(request.public_key, request_raw.public_key);
+        return request;
+    }
+};
+
+/// Accept public kes from the user and assign user a session ID for later communication
+fn register(app: *App, req: *httpz.Request, res: *httpz.Response) !void {
+    const request = RegisterRequest.validateRequest(res.arena, req) catch |err| {
+        std.log.info("422 {} {s} {}", .{ req.method, req.url.path, err });
+        res.status = 422;
+        res.body = "Unprocessable Content";
+        return;
+    };
+    try app.savePublicKey(request.public_key);
+    const sessionId = uuid.v4.new();
     res.status = 200;
-    try res.json(.{ .id = req.param("id").?, .name = "Teg" }, .{});
+    try res.json(.{ .sessionId = uuid.urn.serialize(sessionId) }, .{});
 }
+
+// fn analyze(app: *App, _: *httpz.Request, res: *httpz.Response) !void {
+//     res.status = 200;
+//     try res.json(.{ .name = "Teg" }, .{});
+// }
